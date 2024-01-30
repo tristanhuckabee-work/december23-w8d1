088eb4f7abd456770a00092d38ec1f6d9856bd34
tristan <thuckabee.work@gmail.com>
2024-01-29 18:11:28 -0600

    main 02

--- 02-bind/test.js
+++ 02-bind/test.js
@@ -1,1 +1,13 @@
-// Your code here 
+const Employee = require('./employee.js');
+
+const babaYaga = new Employee('John Wick', 'Dog Lover');
+
+setTimeout( babaYaga.sayName, 2000 );
+// undefined says hello
+setTimeout( babaYaga.sayName.bind(babaYaga), 2000 );
+// John Wick says hello
+
+setTimeout( babaYaga.sayOccupation, 3000);
+// undefined is a undefined
+setTimeout( babaYaga.sayOccupation.bind(babaYaga), 3000);
+// John Wick is a Dog Lover
